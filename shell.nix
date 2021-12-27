decce5d7932f83c675d4599ede6f7086e9941f49
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-27 21:19:16 +0100

    webrtc: Working cross tab communication

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    (python3.withPackages (ps: with ps; [
+      aiohttp
+      python-socketio
+    ]))
+  ];
+}
---
+++ static/index.js
@@ -0,0 +1,143 @@
+(function () {
+  'use strict';
+
+  const SIGNALING_SERVER_URL = 'http://localhost:9999';
+  const PC_CONFIG = {};
+
+  function setupSendMessageHandler(client, channel) {
+    const formEl = document.getElementById(client);
+    formEl.addEventListener('submit', (event) => {
+      console.log('submit');
+      event.preventDefault();
+
+      const textInputEl = formEl.querySelector('input');
+      const message = textInputEl.value;
+
+      textInputEl.value = '';
+      textInputEl.focus();
+
+      channel.send(message);
+
+      const messageEl = document.createElement('p');
+      messageEl.appendChild(document.createTextNode(`> ${message}`));
+
+      const messagesArea = formEl.querySelector('pre');
+      messagesArea.appendChild(messageEl);
+    });
+  }
+
+  const handleChannelStatusChange = (client, channel) => () => {
+    const statusEl = document.querySelector(`#${client} .status`);
+    let state = channel.readyState;
+    if (state === 'open') {
+      statusEl.textContent = 'Connected';
+    } else {
+      statusEl.textContent = 'Disconnected';
+    }
+  }
+
+  function main() {
+    let pc; // Peer connection
+
+    let socket = io(SIGNALING_SERVER_URL, { autoConnect: true });
+
+    socket.on('data', (data) => {
+      console.log('Data received:', data);
+      handleSignalingData(data);
+    });
+
+    function handleSignalingData(data) {
+      switch (data.type) {
+        case 'offer':
+          createPeerConnection();
+          pc.setRemoteDescription(new RTCSessionDescription(data));
+          sendAnswer();
+          break;
+        case 'answer':
+          pc.setRemoteDescription(new RTCSessionDescription(data));
+        case 'candidate':
+          pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+          break;
+      }
+    }
+
+    socket.on('ready', () => {
+      console.log('Ready');
+      createPeerConnection();
+      createDataChannel();
+      sendOffer();
+    });
+
+    function createPeerConnection() {
+      try {
+        pc = new RTCPeerConnection(PC_CONFIG);
+        pc.onconnectionstatechange = (event) => {
+          console.log('Peer connection state:', event);
+        };
+        pc.onicecandidate = onIceCandidate;
+        pc.ondatachannel = (event) => {
+          console.log('Data channel');
+          setDataChannel(event.channel);
+        };
+        console.log('Peer connection created');
+      } catch (error) {
+        console.error('Peer connection failed:', error);
+      }
+    }
+
+    function createDataChannel() {
+      const channel = pc.createDataChannel('chat-channel');
+      setDataChannel(channel);
+    }
+
+    function setDataChannel(channel) {
+      console.log('setDataChannel()');
+      channel.onmessage = (event) => { handleMessage('client', event) };
+      const statusChangeHandler = handleChannelStatusChange('client', channel);
+      channel.onopen = statusChangeHandler;
+      channel.onclose = statusChangeHandler;
+      setupSendMessageHandler('client', channel);
+    }
+
+    function onIceCandidate(event) {
+      if (event.candidate) {
+        console.log('ICE candidate');
+        sendData({
+          type: 'candidate',
+          candidate: event.candidate,
+        });
+      }
+    }
+
+    function sendOffer() {
+      console.log('Send offer');
+      pc.createOffer()
+        .then((sessionDescription) => {
+          pc.setLocalDescription(sessionDescription);
+          sendData(sessionDescription);
+        })
+    }
+
+    function sendAnswer() {
+      pc.createAnswer()
+        .then((sessionDescription) => {
+          pc.setLocalDescription(sessionDescription);
+          sendData(sessionDescription);
+        })
+    }
+
+    function sendData(data) {
+      socket.emit('data', data);
+    }
+
+    function handleMessage(client, event) {
+      const messageEl = document.createElement('p');
+      messageEl.appendChild(document.createTextNode(`< ${event.data}`));
+
+      const messagesArea = document.querySelector(`#${client} pre`);
+      messagesArea.appendChild(messageEl);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => main());
+})();
