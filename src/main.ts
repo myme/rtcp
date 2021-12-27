5ec981659c9b90ae37bb1ca072dc2b8d00416acb
Martin Øinæs Myrseth <myrseth@gmail.com>
2021-12-28 00:30:54 +0100

    webrtc: Fix TypeScript errors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,19 @@ import io from 'socket.io-client';
 const SIGNALING_SERVER_URL = 'http://localhost:9999';
 const PC_CONFIG = {};
 
-function setupSendMessageHandler(client, channel) {
-  const formEl = document.getElementById(client);
-  const handler = (event) => {
+function setupSendMessageHandler(client: string, channel: RTCDataChannel) {
+  const formEl = document.querySelector<HTMLFormElement>(`#${client}`);
+  if (!formEl) {
+    throw new Error('No form element in DOM');
+  }
+  const handler = (event: Event) => {
     console.log('submit');
     event.preventDefault();
 
-    const textInputEl = formEl.querySelector('input');
+    const textInputEl = formEl.querySelector<HTMLInputElement>('input');
+    if (!textInputEl) {
+      throw new Error('No input element in DOM');
+    }
     const message = textInputEl.value;
 
     textInputEl.value = '';
@@ -23,14 +29,17 @@ function setupSendMessageHandler(client, channel) {
     messageEl.appendChild(document.createTextNode(`> ${message}`));
 
     const messagesArea = formEl.querySelector('pre');
-    messagesArea.appendChild(messageEl);
+    messagesArea?.appendChild(messageEl);
   };
   formEl.addEventListener('submit', handler);
   return handler;
 }
 
-const handleChannelStatusChange = (client, channel) => () => {
+const handleChannelStatusChange = (client: string, channel: RTCDataChannel) => () => {
   const statusEl = document.querySelector(`#${client} .status`);
+  if (!statusEl) {
+    throw new Error('No status element in DOM');
+  }
   let state = channel.readyState;
   if (state === 'open') {
     statusEl.textContent = 'Connected';
@@ -40,7 +49,7 @@ const handleChannelStatusChange = (client, channel) => () => {
 }
 
 function main() {
-  let pc; // Peer connection
+  let pc: RTCPeerConnection | null; // Peer connection
 
   let socket = io(SIGNALING_SERVER_URL, { autoConnect: true });
 
@@ -49,12 +58,21 @@ function main() {
     switch (data.type) {
       case 'offer':
         createPeerConnection();
+        if (!pc) {
+          throw new Error('No peer connection');
+        }
         pc.setRemoteDescription(data);
         sendAnswer();
         break;
       case 'answer':
+        if (!pc) {
+          throw new Error('No peer connection');
+        }
         pc.setRemoteDescription(data);
       case 'candidate':
+        if (!pc) {
+          throw new Error('No peer connection');
+        }
         if (data.candidate) {
           pc.addIceCandidate(data.candidate);
         }
@@ -87,28 +105,36 @@ function main() {
   }
 
   function createDataChannel() {
+    if (!pc) {
+      return;
+    }
     const channel = pc.createDataChannel('chat-channel');
     setDataChannel(channel);
   }
 
-  function setDataChannel(channel) {
+  function setDataChannel(channel: RTCDataChannel) {
     console.log('setDataChannel()');
     channel.onmessage = (event) => { handleMessage('client', event) };
     const statusChangeHandler = handleChannelStatusChange('client', channel);
     channel.onopen = statusChangeHandler;
-    let handler;
+    let handler: (event: Event) => void;
     channel.onclose = () => {
       statusChangeHandler();
       console.log('Closing Peer Connection');
-      pc.close();
-      pc = null;
+      if (pc) {
+        pc.close();
+        pc = null;
+      }
       const formEl = document.getElementById('client');
+      if (!formEl) {
+        throw new Error('No form element in DOM');
+      }
       formEl.removeEventListener('submit', handler);
     };
     handler = setupSendMessageHandler('client', channel);
   }
 
-  function onIceCandidate(event) {
+  function onIceCandidate(event: RTCPeerConnectionIceEvent) {
     if (event.candidate) {
       console.log('ICE candidate');
       sendData({
@@ -118,32 +144,38 @@ function main() {
     }
   }
 
-  function sendOffer() {
+  async function sendOffer() {
     console.log('Send offer');
-    pc.createOffer()
-      .then((sessionDescription) => {
-        pc.setLocalDescription(sessionDescription);
-        sendData(sessionDescription);
-      })
+    if (!pc) {
+      throw new Error('No peer connection');
+    }
+    const sessionDescription = await pc.createOffer();
+    pc.setLocalDescription(sessionDescription);
+    sendData(sessionDescription);
   }
 
-  function sendAnswer() {
-    pc.createAnswer()
-      .then((sessionDescription) => {
-        pc.setLocalDescription(sessionDescription);
-        sendData(sessionDescription);
-      })
+  async function sendAnswer() {
+    console.log('Send answer');
+    if (!pc) {
+      throw new Error('No peer connection');
+    }
+    const sessionDescription = await pc.createAnswer();
+    pc.setLocalDescription(sessionDescription);
+    sendData(sessionDescription);
   }
 
-  function sendData(data) {
+  function sendData(data: object) {
     socket.emit('data', data);
   }
 
-  function handleMessage(client, event) {
+  function handleMessage(client: string, event: MessageEvent<any>) {
     const messageEl = document.createElement('p');
     messageEl.appendChild(document.createTextNode(`< ${event.data}`));
 
     const messagesArea = document.querySelector(`#${client} pre`);
+    if (!messagesArea) {
+      throw new Error('No messages area in DOM');
+    }
     messagesArea.appendChild(messageEl);
   }
 }
