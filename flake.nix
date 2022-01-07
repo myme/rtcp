f65addca0acfe7d7296827e4c49052434c6a2f4c
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-07 15:20:04 +0100

    nix: Move dev script into flake

--- flake.nix
+++ flake.nix
@@ -15,10 +15,24 @@
     in {
       overlay = (final: prev: {
         xchg-frontend = final.callPackage ./frontend { nodejs = final.nodejs; };
-        xchg-backend = final.haskellPackages.callCabal2nix "xchg-backend" ./server {};
+        xchg-server = final.haskellPackages.callCabal2nix "xchg-server" ./server {};
+      });
+      apps = forAllSystems (system: {
+        dev =
+          let pkgs = nixpkgsFor.${system};
+          in pkgs.writeShellScriptBin "dev" ''
+            rm -rf ./node_modules
+            ln -s ${pkgs.xchg-frontend.nodeDependencies}/lib/node_modules ./node_modules
+            export PATH="${pkgs.xchg-frontend.nodeDependencies}/bin:$PATH"
+            npx concurrently \
+                -n FE,BE \
+                -c green,red \
+                "cd frontend && npm run frontend" \
+                "cd server && ghcid -r Main"
+          '';
       });
       packages = forAllSystems (system: {
-        xchg-frontend =
+        frontend =
           let pkgs = nixpkgsFor.${system};
           in pkgs.stdenv.mkDerivation {
             name = "xchg-frontend";
@@ -33,7 +47,7 @@
               cp -r dist $out/
             '';
           };
-        xchg-backend = nixpkgsFor.${system}.xchg-backend;
+        server = nixpkgsFor.${system}.xchg-server;
       });
       defaultPackage = forAllSystems (system: self.packages.${system}.xchg);
       checks = self.packages;
@@ -55,12 +69,13 @@
         let pkgs = nixpkgsFor.${system};
             haskellPackages = pkgs.haskellPackages;
         in {
-          frontend = pkgs.xchg-frontend.shell;
+          frontend = pkgs.xchg-frontend.shell.override {
+            buildInputs = [pkgs.nodePackages.node2nix];
+          };
           server = haskellPackages.shellFor {
-            packages = p: [self.packages.${system}.xchg-backend];
+            packages = p: [pkgs.xchg-server];
             withHoogle = true;
             buildInputs = with haskellPackages; [
-              pkgs.nodePackages.node2nix
               haskell-language-server
               ghcid
               cabal-install
