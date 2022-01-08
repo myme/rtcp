a98a9caa4bb2e047e13670f72fcf971434d69787
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-08 12:14:30 +0100

    nix: Clean up flake

--- flake.nix
+++ flake.nix
@@ -12,59 +12,60 @@
         inherit system;
         overlays = [ self.overlay ];
       });
+
+      # https://www.reddit.com/r/NixOS/comments/ce9cmu/comment/eu1c7sh/?utm_source=share&utm_medium=web2x&context=3
+      # https://gist.github.com/adisbladis/2a44cded73e048458a815b5822eea195
+      mergeEnvs = pkgs: envs: pkgs.mkShell (builtins.foldl' (a: v: {
+        buildInputs = a.buildInputs ++ v.buildInputs;
+        nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
+        propagatedBuildInputs = a.propagatedBuildInputs ++ v.propagatedBuildInputs;
+        propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ v.propagatedNativeBuildInputs;
+        shellHook = a.shellHook + "\n" + v.shellHook;
+      }) (pkgs.mkShell {}) envs);
+
+      # Launch development server
+      dev = pkgs: pkgs.writeShellScriptBin "dev" ''
+        rm -rf ./node_modules
+        ln -s ${pkgs.xchg-frontend.nodeDependencies}/lib/node_modules ./node_modules
+        export PATH="${pkgs.xchg-frontend.nodeDependencies}/bin:$PATH"
+        npx concurrently \
+            -n FE,BE \
+            -c green,red \
+            "cd frontend && npm run frontend" \
+            "cd server && ghcid -r Main"
+      '';
+
+      # Static frontend assets
+      static = pkgs: pkgs.stdenv.mkDerivation {
+        name = "xchg-frontend";
+        src = ./frontend;
+        buildInputs = [pkgs.nodejs];
+        buildPhase = ''
+          ln -s ${pkgs.xchg-frontend.nodeDependencies}/lib/node_modules ./node_modules
+          export PATH="${pkgs.xchg-frontend.nodeDependencies}/bin:$PATH"
+          npm run build
+        '';
+        installPhase = ''
+          cp -r dist $out/
+        '';
+      };
+
     in {
       overlay = (final: prev: {
         xchg-frontend = final.callPackage ./frontend { nodejs = final.nodejs; };
         xchg-server = final.haskellPackages.callCabal2nix "xchg-server" ./server {};
       });
       apps = forAllSystems (system: {
-        dev =
-          let pkgs = nixpkgsFor.${system};
-          in pkgs.writeShellScriptBin "dev" ''
-            rm -rf ./node_modules
-            ln -s ${pkgs.xchg-frontend.nodeDependencies}/lib/node_modules ./node_modules
-            export PATH="${pkgs.xchg-frontend.nodeDependencies}/bin:$PATH"
-            npx concurrently \
-                -n FE,BE \
-                -c green,red \
-                "cd frontend && npm run frontend" \
-                "cd server && ghcid -r Main"
-          '';
+        dev = dev nixpkgsFor.${system};
       });
       packages = forAllSystems (system: {
-        frontend =
-          let pkgs = nixpkgsFor.${system};
-          in pkgs.stdenv.mkDerivation {
-            name = "xchg-frontend";
-            src = ./frontend;
-            buildInputs = [pkgs.nodejs];
-            buildPhase = ''
-              ln -s ${pkgs.xchg-frontend.nodeDependencies}/lib/node_modules ./node_modules
-              export PATH="${pkgs.xchg-frontend.nodeDependencies}/bin:$PATH"
-              npm run build
-            '';
-            installPhase = ''
-              cp -r dist $out/
-            '';
-          };
+        static = static nixpkgsFor.${system};
         server = nixpkgsFor.${system}.xchg-server;
       });
       defaultPackage = forAllSystems (system: self.packages.${system}.xchg);
       checks = self.packages;
-      devShell =
-        # https://www.reddit.com/r/NixOS/comments/ce9cmu/comment/eu1c7sh/?utm_source=share&utm_medium=web2x&context=3
-        # https://gist.github.com/adisbladis/2a44cded73e048458a815b5822eea195
-        forAllSystems (system:
-          let pkgs = nixpkgsFor.${system};
-              mergeEnvs = envs: pkgs.mkShell (builtins.foldl' (a: v: {
-                buildInputs = a.buildInputs ++ v.buildInputs;
-                nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-                propagatedBuildInputs = a.propagatedBuildInputs ++ v.propagatedBuildInputs;
-                propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ v.propagatedNativeBuildInputs;
-                shellHook = a.shellHook + "\n" + v.shellHook;
-              }) (pkgs.mkShell {}) envs);
-          in mergeEnvs (with self.devShells.${system}; [frontend server])
-        );
+      devShell = forAllSystems (system:
+        mergeEnvs nixpkgsFor.${system} (with self.devShells.${system}; [frontend server]));
       devShells = forAllSystems (system:
         let pkgs = nixpkgsFor.${system};
             haskellPackages = pkgs.haskellPackages;
