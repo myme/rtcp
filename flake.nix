3ea8e5f6aa2825bcf56bb108ea2b0645004c8a7e
Martin Øinæs Myrseth <myrseth@gmail.com>
2022-01-13 21:07:52 +0100

    nix: Use numtide/flake-utils

--- flake.nix
+++ flake.nix
@@ -3,59 +3,48 @@
   description = "xchg - Exchange stuff between things using WebRTC";
 
   inputs.nixpkgs.url = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
+          overlays = [ overlay ];
+        };
+        overlay = (final: prev: {
+          xchg = (final.callPackage ./. { } // {
+            server = final.callPackage ./server { };
+            frontend = final.callPackage ./frontend { };
+          });
         });
 
-      # https://www.reddit.com/r/NixOS/comments/ce9cmu/comment/eu1c7sh/?utm_source=share&utm_medium=web2x&context=3
-      # https://gist.github.com/adisbladis/2a44cded73e048458a815b5822eea195
-      mergeEnvs = pkgs: envs:
-        pkgs.mkShell (builtins.foldl' (a: v: {
-          buildInputs = a.buildInputs ++ v.buildInputs;
-          nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-          propagatedBuildInputs = a.propagatedBuildInputs
-            ++ v.propagatedBuildInputs;
-          propagatedNativeBuildInputs = a.propagatedNativeBuildInputs
-            ++ v.propagatedNativeBuildInputs;
-          shellHook = a.shellHook + "\n" + v.shellHook;
-        }) (pkgs.mkShell { }) envs);
+        # https://www.reddit.com/r/NixOS/comments/ce9cmu/comment/eu1c7sh/?utm_source=share&utm_medium=web2x&context=3
+        # https://gist.github.com/adisbladis/2a44cded73e048458a815b5822eea195
+        mergeEnvs = pkgs: envs:
+          pkgs.mkShell (builtins.foldl' (a: v: {
+            buildInputs = a.buildInputs ++ v.buildInputs;
+            nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
+            propagatedBuildInputs = a.propagatedBuildInputs
+              ++ v.propagatedBuildInputs;
+            propagatedNativeBuildInputs = a.propagatedNativeBuildInputs
+              ++ v.propagatedNativeBuildInputs;
+            shellHook = a.shellHook + "\n" + v.shellHook;
+          }) (pkgs.mkShell { }) envs);
 
-    in {
-      overlay = (final: prev: {
-        xchg = (final.callPackage ./. { } // {
-          server = final.callPackage ./server { };
-          frontend = final.callPackage ./frontend { };
-        });
-      });
-      apps =
-        forAllSystems (system: { dev = nixpkgsFor.${system}.xchg.dev; });
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
+      in rec {
+        apps = { dev = pkgs.xchg.dev; };
+        packages = {
           image = pkgs.xchg.image;
           server = pkgs.xchg.server.server;
           static = pkgs.xchg.frontend.static;
-        });
-      defaultPackage = forAllSystems (system: self.packages.${system}.image);
-      checks = self.packages;
-      devShell = forAllSystems (system:
-        mergeEnvs nixpkgsFor.${system}
-        (with self.devShells.${system}; [ frontend server ]));
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-          haskellPackages = pkgs.haskellPackages;
-        in {
+        };
+        defaultPackage = packages.image;
+        checks = packages;
+        devShell = mergeEnvs pkgs (with devShells; [ frontend server ]);
+        devShells = {
           frontend = pkgs.xchg.frontend.shell;
           server = pkgs.xchg.server.shell;
-        });
-    };
+        };
+      });
 }
