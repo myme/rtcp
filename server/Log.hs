08b9628cda50efd3e9006722acd01bccc023e1d6
Martin Myrseth <mm@myme.no>
2023-01-06 22:28:55 +0100

    backend: Auto-format

--- server/Log.hs
+++ server/Log.hs
@@ -1,7 +1,8 @@
 module Log
- ( info
- , err
- ) where
+  ( info,
+    err,
+  )
+where
 
 import System.IO (hPutStrLn, stderr)
 
--- server/Messages.hs
+++ server/Messages.hs
@@ -2,11 +2,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Messages
- ( Request(..)
- , RequestWithId(..)
- , Response(..)
- , stripPrefix
- ) where
+  ( Request (..),
+    RequestWithId (..),
+    Response (..),
+    stripPrefix,
+  )
+where
 
 import Data.Aeson (FromJSON, Options (allNullaryToStringTag, constructorTagModifier, fieldLabelModifier, sumEncoding), ToJSON, (.:))
 import qualified Data.Aeson as JSON
@@ -19,43 +20,47 @@ stripPrefix :: String -> String -> String
 stripPrefix prefix text = fromMaybe text $ List.stripPrefix prefix text
 
 requestJSONOptions :: Options
-requestJSONOptions = JSON.defaultOptions
-  { allNullaryToStringTag = False
-  , constructorTagModifier = \(c:cs) -> toLower c : cs
-  , fieldLabelModifier = stripPrefix "request_"
-  , sumEncoding = JSON.TaggedObject "method" "method"
-  }
+requestJSONOptions =
+  JSON.defaultOptions
+    { allNullaryToStringTag = False,
+      constructorTagModifier = \(c : cs) -> toLower c : cs,
+      fieldLabelModifier = stripPrefix "request_",
+      sumEncoding = JSON.TaggedObject "method" "method"
+    }
 
 data RequestWithId = RequestWithId
-  { request_id :: Int
-  , request :: Request
-  } deriving Show
+  { request_id :: Int,
+    request :: Request
+  }
+  deriving (Show)
 
 instance FromJSON RequestWithId where
   parseJSON = JSON.withObject "RequestWithId" $ \obj ->
     RequestWithId <$> (obj .: "id") <*> JSON.parseJSON (JSON.toJSON obj)
 
-data Request = GetIceConfig { request_hostname :: String }
-             | NewSession
-             | JoinSession { request_sessionId :: String }
-             | LeaveSession
-             | Broadcast { request_params :: JSON.Value }
-             deriving (Generic, Show)
+data Request
+  = GetIceConfig {request_hostname :: String}
+  | NewSession
+  | JoinSession {request_sessionId :: String}
+  | LeaveSession
+  | Broadcast {request_params :: JSON.Value}
+  deriving (Generic, Show)
 
 instance FromJSON Request where
   parseJSON = JSON.genericParseJSON requestJSONOptions
 
-data Response = Success { response_id :: Maybe Int, response_result :: JSON.Value }
-
-              | Notify { response_method :: String, response_params :: Maybe JSON.Value }
-              | Failure { response_id :: Maybe Int, response_error :: String }
+data Response
+  = Success {response_id :: Maybe Int, response_result :: JSON.Value}
+  | Notify {response_method :: String, response_params :: Maybe JSON.Value}
+  | Failure {response_id :: Maybe Int, response_error :: String}
   deriving (Generic, Show)
 
 responseJSONOptions :: Options
-responseJSONOptions = JSON.defaultOptions
-  { fieldLabelModifier = stripPrefix "response_"
-  , sumEncoding = JSON.UntaggedValue
-  }
+responseJSONOptions =
+  JSON.defaultOptions
+    { fieldLabelModifier = stripPrefix "response_",
+      sumEncoding = JSON.UntaggedValue
+    }
 
 instance ToJSON Response where
   toJSON = JSON.genericToJSON responseJSONOptions
