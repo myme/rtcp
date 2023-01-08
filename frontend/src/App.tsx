3cceaf704452911a75dd23b5aa98371baafdfb07
Martin Myrseth <mm@myme.no>
2023-01-08 16:20:11 +0100

    app: Remove some useCallbacks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import * as uuid from 'uuid';
 
@@ -21,46 +21,44 @@ export default function App() {
   const [session, setSession] = useState<Session>();
   const peerConnection = usePeerConnection();
 
-  const addShare = useCallback((direction: Direction, item: Item) => {
+  const addShare = (direction: Direction, item: Item) => {
     const id = uuid.v4();
     const share = { id, direction, item };
     setShares(shares => shares.concat(share));
     return share;
-  }, [setShares]);
+  };
 
-  const addIncomingShare = useCallback((share: IShare) => {
+  const addIncomingShare = (share: IShare) => {
     setShares(shares => shares.concat(share));
-  }, [setShares]);
+  };
 
-  const onCopyItem = useCallback((id: string) => {
+  const onCopyItem = (id: string) => {
     const share = shares.find(share => share.id === id);
-    if (!share) {
-      return;
-    }
+    if (!share) return;
     navigator.clipboard.writeText(share.item.value);
-  }, [shares]);
+  };
 
-  const onRemoveLocalShare = useCallback((id: string) => {
+  const onRemoteShareRemoved = (id: string) => {
+    setShares(shares => shares.filter(share => share.id !== id));
+  };
+
+  const onRemoveLocalShare = (id: string) => {
     setShares(shares => shares.filter(share => share.id !== id));
     if (!peerConnection) {
       logger.error('App::onLocalRemoveShare(): No peer connection');
       return;
     }
     peerConnection.removeShare(id);
-  }, [peerConnection, setShares]);
+  };
 
-  const onRemoteShareRemoved = useCallback((id: string) => {
-    setShares(shares => shares.filter(share => share.id !== id));
-  }, [setShares]);
-
-  const onSend = useCallback((item: Item) => {
+  const onSend = (item: Item) => {
     const share = addShare('outbound', item);
     if (!peerConnection) {
       logger.error('App::onSend(): No peer connection');
       return;
     }
     peerConnection.sendShare(share);
-  }, [addShare, peerConnection]);
+  };
 
   return (
     <Routes>
